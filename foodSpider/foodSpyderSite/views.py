732ec2362fc91a966b81d2ad247654a99a391b4a
udupashreyas <udupa_shreyas@yahoo.co.in>
2017-01-14 00:50:32 -0800

    front end stuff

--- foodSpider/foodSpyderSite/views.py
+++ foodSpider/foodSpyderSite/views.py
@@ -1,6 +1,15 @@
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_protect
+#from foodSpyderSite.forms import *
 
+from .models import Recipe, Ingredient, UserInput
+
+@csrf_protect
 def index(request):
-	return HttpResponse("Food Spyder is up!!")
-
-# Create your views here.
+	matched_recipes = []
+	if request.method == 'POST':
+		if 'inputRecipe' in request.POST:
+			matched_recipes = ['bisibelebath']
+	return render_to_response('index.html', {'matches':matched_recipes}, RequestContext(request))
